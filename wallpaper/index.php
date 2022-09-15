71985307bf0700489d964b668f90346c76ce1065
vvebo <vvebo@noreply.gitea.io>
2022-09-15 21:03:44 +0800

    更新 'wallpaper/index.php'

--- wallpaper/index.php
+++ wallpaper/index.php
@@ -3,9 +3,12 @@
 <body>
 
 <?php 
-$myLinks = array("https://gitea.com/00700/tomato/raw/branch/main/wallpaper/01.png", 
-    "https://gitea.com/00700/tomato/raw/branch/main/wallpaper/02.png",
-    "https://gitea.com/00700/tomato/raw/branch/main/wallpaper/03.png");
+$myLinks = array("https://gitea.com/vvebo/vvebo/raw/branch/main/wallpaper/01.png", 
+    "https://gitea.com/vvebo/vvebo/raw/branch/main/wallpaper/02.png",
+    "https://gitea.com/vvebo/vvebo/raw/branch/main/wallpaper/03.png",
+    "https://gitea.com/vvebo/vvebo/raw/branch/main/wallpaper/04.png",
+    "https://gitea.com/vvebo/vvebo/raw/branch/main/wallpaper/05.png",
+    "https://gitea.com/vvebo/vvebo/raw/branch/main/wallpaper/06.png");
 
 $randomRedirection = $myLinks[array_rand($myLinks)];
 header("Location: $randomRedirection");
