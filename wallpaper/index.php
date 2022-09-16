b6642204316936ed76e1c76c0ee51d4827b9f507
00700 <00700@tutanota.com>
2022-09-16 10:18:15 +0000

    更新 'wallpaper/index.php'

--- wallpaper/index.php
+++ wallpaper/index.php
@@ -3,12 +3,18 @@
 <body>
 
 <?php 
-$myLinks = array("https://gitea.com/vvebo/vvebo/raw/branch/main/wallpaper/01.png", 
-    "https://gitea.com/vvebo/vvebo/raw/branch/main/wallpaper/02.png",
-    "https://gitea.com/vvebo/vvebo/raw/branch/main/wallpaper/03.png",
-    "https://gitea.com/vvebo/vvebo/raw/branch/main/wallpaper/04.png",
-    "https://gitea.com/vvebo/vvebo/raw/branch/main/wallpaper/05.png",
-    "https://gitea.com/vvebo/vvebo/raw/branch/main/wallpaper/06.png");
+$myLinks = array("https://agit.ai/00700/vvebo/raw/branch/main/wallpaper/01.png", 
+    "https://agit.ai/00700/vvebo/raw/branch/main/wallpaper/02.png",
+    "https://agit.ai/00700/vvebo/raw/branch/main/wallpaper/03.png",
+    "https://agit.ai/00700/vvebo/raw/branch/main/wallpaper/04.png",
+    "https://agit.ai/00700/vvebo/raw/branch/main/wallpaper/05.png",
+    "https://agit.ai/00700/vvebo/raw/branch/main/wallpaper/06.png",
+    "https://agit.ai/00700/vvebo/raw/branch/main/wallpaper/07.png",
+    "https://agit.ai/00700/vvebo/raw/branch/main/wallpaper/08.png",
+    "https://agit.ai/00700/vvebo/raw/branch/main/wallpaper/09.png",
+    "https://agit.ai/00700/vvebo/raw/branch/main/wallpaper/10.png",
+    "https://agit.ai/00700/vvebo/raw/branch/main/wallpaper/12.png",
+    "https://agit.ai/00700/vvebo/raw/branch/main/wallpaper/13.png");
 
 $randomRedirection = $myLinks[array_rand($myLinks)];
 header("Location: $randomRedirection");
